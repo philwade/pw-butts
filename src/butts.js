a52efdfadc8d20123741a6b4e90a322820aca945
Phil Wade <phil@whoisstudio.com>
2016-05-24 13:44:03 -0400

    Configurable butts.

--- src/butts.js
+++ src/butts.js
@@ -1,12 +1,34 @@
 'use strict';
 
 angular.module('pw-butts', [])
-	.directive('butt', function() {
-		return {
-			restrict: 'E',
-			template: '(_I_)'
+	.provider('buttFactory', function() {
+		 var butts = {
+			default: '(_I_)',
+		};
+
+		this.setButt = function(value, name) {
+			if(typeof name === 'undefined') {
+				butts.default = value;
+			}
+			else {
+				butts[name] = value;
+			}
+		};
+
+		this.$get = function() {
+			return {
+				getButt: function(name) {
+					return typeof butts[name] === 'undefined' ? butts.default : butts[name];
+				},
+			};
 		};
 	})
+	.directive('butt', ['buttFactory', function(buttFactory) {
+		return {
+			restrict: 'E',
+			template: buttFactory.getButt()
+		};
+	}])
 	.directive('butts', function() {
 		return {
 			restrict: 'E',
